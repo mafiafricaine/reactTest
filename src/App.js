a53b5539936435b8050dfa3371ffe5ff1dadd4d9
juldblack <juliendunia@hotmail.com>
2023-02-10 09:55:50 +0100

    Voici le site avec ph-pages pour la page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import image from './goku.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Welcome chez Goku</h1>
+      <img src={image} width="200" height={200} alt="image" />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
